96b1524ff73169b9452216e1b249a607e6d7fa66
DaguerMuebleria <muebleriadaguerr@outlook.com>
2026-08-03 01:51:58 -0600

    23

--- js/Tubulares.js
+++ js/Tubulares.js
@@ -1,2 +1,21 @@
 const productosTubulares = [
+
+  {
+    id: 4000,
+    titulo: "ejemplo",
+    categoria: "Tubulares",
+    subcategoria: "",
+    estado: "disponible",
+    descripcion: `• ejemplo
+• ejemplo ejmeplo
+• ejemplo
+• ejmepl`,
+    oculto: false, 
+    imagenes: [
+      "Imagenes/Tubulares/CarpetaProducto/1.jpg",
+      "Imagenes/Tubulares/CarpetaProducto/2.jpg",
+      "Imagenes/Tubulares/CarpetaProducto/3.jpg",
+      "Imagenes/Tubulares/CarpetaProducto/4.jpg"
+    ]
+  },
 ];
