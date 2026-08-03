995cb166bb236426cd036f3a695f10f4468c28d6
DaguerMuebleria <muebleriadaguerr@outlook.com>
2026-08-03 10:23:22 -0600

    Actualizacion Daguerr

--- js/Comedores.js
+++ js/Comedores.js
@@ -1,18 +1,2 @@
 const productosComedores = [
-
-  {
-    id: 3000,
-    titulo: "ejemplo",
-    categoria: "Comedores",
-    subcategoria: "",
-    estado: "disponible",
-    descripcion: `hola esto es una prueba`,
-    oculto: false, 
-    imagenes: [
-      "Imagenes/Comedores/CarpetaProducto/1.jpg",
-      "Imagenes/Comedores/CarpetaProducto/2.jpg",
-      "Imagenes/Comedores/CarpetaProducto/3.jpg",
-      "Imagenes/Comedores/CarpetaProducto/4.jpg"
-    ]
-  },
 ];
