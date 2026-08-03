9e96378c060ceb10e13ea64f3b01348d65b777f2
DaguerMuebleria <muebleriadaguerr@outlook.com>
2026-08-03 01:13:56 -0600

    Actualizacion Colchon

--- js/Colchoneria.js
+++ js/Colchoneria.js
@@ -1,2 +1,16 @@
 const productosColchoneria = [
+
+  {
+    id: 1000,
+    titulo: "colchon duro",
+    categoria: "Colchoneria",
+    subcategoria: "",
+    estado: "disponible",
+    descripcion: "• jajaja\n• lero\n• lero lero",
+    oculto: false, 
+    imagenes: [
+      "Imagenes/LineaBlanca/LavMabe24k757638389785/1.jpg",
+      "Imagenes/LineaBlanca/LavMabe24k757638389785/2.jpg"
+    ]
+  },
 ];
