57e82431836cc4c47e59cc6e0436aa22aa8485c3
DaguerMuebleria <muebleriadaguerr@outlook.com>
2026-08-03 12:41:16 -0600

    nueva actualizacion

--- js/Closet.js
+++ js/Closet.js
@@ -1,18 +1,2 @@
 const productosCloset = [
-
-  {
-    id: 6000,
-    titulo: "ejemplo",
-    categoria: "Closet",
-    subcategoria: "",
-    estado: "disponible",
-    descripcion: ``,
-    oculto: false, 
-    imagenes: [
-      "Imagenes/Closet/CarpetaProducto/1.jpg",
-      "Imagenes/Closet/CarpetaProducto/2.jpg",
-      "Imagenes/Closet/CarpetaProducto/3.jpg",
-      "Imagenes/Closet/CarpetaProducto/4.jpg"
-    ]
-  },
 ];
